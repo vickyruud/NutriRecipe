9fc469fc84aea30687844805fed6f633d3b3675a
vickyruud <aprajit.k@gmail.com>
2022-02-09 23:38:40 -0500

    fix user login and signup

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,50 +5,51 @@ import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
 import axios from 'axios';
 
 
-const Login = () => {
+const Login = (props) => {
   const paperStyle = { padding: '30px 20px', width: 300, margin: '20px auto' };
   const headerStyle = { margin: 0 };
   const avatarStyle = { backgroundColor: '#0068d3' };
   const textFieldStyle = { margin: '10px 0 0 0' };
   const loginButtonStyle = {margin: '10px 0 0 0'}
 
-  const [user, setUser] = useState('');
-  
-  const userLogin = (user) => {
-    axios.get('/auto_login', {
-      headers: {
-        "content-type": "application/json"
-      },
-      body: {email: user.email, password: user.password } 
-    }, user) 
-    .then((resp) => {
-      // handle success
-      localStorage.setItem("token", resp.data.token)
-      setUser(resp.data.user);
-      console.log(resp.data);
-    })
-   
-  }
-  useEffect(() => {
-    if (localStorage.getItem("token")) {
-      axios.post('/login', {
-        headers: {"Authenticate": localStorage.token}
-      })
-        .then(resp => {
-          setUser(user);
-      })
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+    const handleUsernameChange = (evt) => {
+        setUsername(evt.target.value)
     }
-  })
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const user = {
-      username: event.target[0].value,
-      password: event.target[1].value
-    } 
-    userLogin(user)
-  }
+    const handlePasswordChange = (evt) => {
+        setPassword(evt.target.value)
+    }
+   
 
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        fetch(`http://localhost:3000/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+              username,
+              password,
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            localStorage.setItem("token", data.jwt)
+            props.handleLogin(data.user)
+        })
+      setUsername("")
+      setPassword("")
+    }
+    
+  
+  
+
+  
   return (
     <Grid>
       <Paper elevation={20} style = {paperStyle} id="login-paper">
@@ -59,9 +60,9 @@ const Login = () => {
           <h2 style ={headerStyle}>Login</h2>
         </Grid>
         <form className='login-form' onSubmit={handleSubmit}>
-          <TextField style={textFieldStyle} variant='standard' fullWidth label='Username' placeholder='Enter your username' />
-          <TextField style={textFieldStyle} variant='standard' fullWidth label='Password' type="password" placeholder='Enter a password' />
-          <Button style={loginButtonStyle} type='submit' variant='contained' color='primary'>Login in</Button>
+          <TextField onChange={handleUsernameChange} style={textFieldStyle} variant='standard' fullWidth label='Username' placeholder='Enter your username' />
+          <TextField onChange={handlePasswordChange} style={textFieldStyle} variant='standard' fullWidth label='Password' type="password" placeholder='Enter a password' />
+          <Button style={loginButtonStyle} type='submit' variant='contained' color='primary'>Login</Button>
           
         </form>
       </Paper>
