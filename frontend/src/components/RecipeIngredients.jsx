63c73be031160c46a4ffee442581c6dba4a88028
Jhanvi <83320700+JhanviShah1@users.noreply.github.com>
2022-02-11 11:19:47 -0500

    Recipe page details

---
+++ frontend/src/components/RecipeIngredients.jsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import RecipeIngredientsNames from './RecipeIngredientsNames'
+
+const columns = [
+  {
+    field: 'name',
+    headerClassName: 'super-app-theme--header',
+    headerAlign: 'center',
+    width: 140,
+  },
+  {
+    field: 'unit',
+    headerClassName: 'super-app-theme--header',
+    headerAlign: 'center',
+    width: 140,
+  },
+  {
+    field: 'quantity',
+    headerClassName: 'super-app-theme--header',
+    headerAlign: 'center',
+    width: 140,
+  },
+];
+
+const rows = [
+  {
+    id  : 1,
+    name: 1,
+    unit: 'Carrots',
+    quantity: '1234'
+  },
+  {
+    id: 2,
+    Ingredients: 'Milk',
+    Quantity: '124'
+  },
+  {
+    id: 3,
+    Ingredients: 'Oats',
+    Quantity: '1'
+  },
+];
+// // -look inside the database for ingredints
+// -pass the ingredient id to the frontend
+// -once the unique id exists in the ingrdientobj that is what we want to achieve 
+export default function StylingHeaderGrid(props) {
+  const ingredientObj = eval(props.list.ingredients);
+  return (
+    <div>
+    {ingredientObj.map(ingredient=> 
+      
+    
+            <RecipeIngredientsNames {...ingredient} />
+ 
+      
+      )}
+      {console.log("OBJ====>",ingredientObj)}
+    <Box
+      sx={{
+        height: 300,
+        width: 1,
+        '& .super-app-theme--header': {
+          backgroundColor: 'rgba(255, 7, 0, 0.55)',
+        },
+      }}
+    >
+      <DataGrid rows={ingredientObj} columns={columns} />
+    </Box>
+    </div>
+  );
+}
+
+
+
+
+
+  
+    
+  
+  
+
--- frontend/src/routes/recipes.jsx
+++ frontend/src/routes/recipes.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import NavBar from "../components/NavBar";
 import RecipeList from "../components/RecipeList";
 import "../App.css";
 import RecipePage from "../components/RecipePage";
 
 export default function Recipes() {
-  const [message, setMessage] = useState("");
   const [recipes, setRecipes] = useState([]);
   const [selectRecipe, setSelectRecipe] = useState(null);
   const fetchRecipes = () => {
@@ -17,23 +15,20 @@ export default function Recipes() {
         // handle success
         console.log(response.data[0].name); // The entire response from the Rails API
 
-        // setMessage(response.data[0].name);
         setRecipes(response.data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
-  console.log("selectrecipe====>", selectRecipe);
-  console.log("recipes====>", recipes);
+  // console.log("selectrecipe====>", selectRecipe);
+  // console.log("recipes====>", recipes);
 
   useEffect(() => {
     fetchRecipes();
   }, []);
   return (
     <main>
-      {/* <NavBar login_name={"Final Project"} login_right={1} /> */}
-      <h2>{message}</h2>
       <div style={{ display: "flex", flexDirection: "row" }}></div>
       {selectRecipe ? (
         <RecipePage selectRecipe={selectRecipe} />
