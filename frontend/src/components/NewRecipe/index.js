e7f5cc46f5d08ff7842dafaebd49402266af492c
Anh Tong <tongkimanh@yahoo.com>
2022-02-14 21:45:49 +0000

    To fix merge issue

--- frontend/src/components/NewRecipe/index.js
+++ frontend/src/components/NewRecipe/index.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Form from './Form'
-import Show from './Show'; // Recipe detail page
+// import Show from './Show'; // Recipe detail page
 import Show from '../RecipePage1'; // Recipe detail page
-// import Empty from '../RecipeList'; // Main page
+import Empty from '../RecipeList'; // Main page
 import Status from './Status';
 import Confirm from './Confirm';
 import Error from './Error';
