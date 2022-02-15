c953820d4ad76145178efb0439d7daffb65eb86c
Jhanvi <83320700+JhanviShah1@users.noreply.github.com>
2022-02-15 11:19:36 -0500

    Comments component

--- frontend/src/components/RecipePage1.jsx
+++ frontend/src/components/RecipePage1.jsx
@@ -1,47 +1,64 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import IngredientTable from "./IngredientTable"
-import RecipeSteps from './RecipeSteps';
-import NutriContent from './NutriContent';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import IngredientTable from "./IngredientTable";
+import RecipeSteps from "./RecipeSteps";
+import NutriContent from "./NutriContent";
 import DoughnutChart from "../charts/Doughnut";
-import { Card } from '@mui/material';
-import './recipePage.css'
-
+import { Card } from "@mui/material";
+import "./recipePage.css";
+import Comments from "./Comments";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
   border: "none",
-  boxShadow: "none"
+  boxShadow: "none",
 }));
 
 export default function Recipes(props) {
+  // console.log("this are comments", props.comments);
   return (
-    <Box sx={{ width: '100%'}} >
-      <Grid style={{outline:'none',border: 0}}container rowSpacing={-2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+    <Box sx={{ width: "100%" }}>
+      <Grid
+        style={{ outline: "none", border: 0 }}
+        container
+        rowSpacing={-2}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      >
         <Grid item xs={6}>
-        <h1> {props.selectRecipe.name} </h1>
-          <Item><IngredientTable list={props.selectRecipe} /> 
+          <h1> {props.selectRecipe.name} </h1>
+          <Item>
+            <IngredientTable list={props.selectRecipe} />
           </Item>
         </Grid>
         <Grid item xs={6}>
           <h3>Nutrition Content</h3>
-          <Item><DoughnutChart /></Item>
+          <Item>
+            <DoughnutChart />
+          </Item>
         </Grid>
-        <Grid style={{padding: 5}} item xs={6}>
-          <Item><RecipeSteps list={props.selectRecipe}/></Item>
+        <Grid style={{ padding: 5 }} item xs={6}>
+          <Item>
+            <RecipeSteps list={props.selectRecipe} />
+          </Item>
         </Grid>
         <Grid item xs={6}>
-          <Item><NutriContent list = {props.selectRecipe}/></Item>
+          <Item>
+            <NutriContent list={props.selectRecipe} />
+          </Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>
+            <Comments comments={props.comments} />
+          </Item>
         </Grid>
       </Grid>
       {/* <NutriContent list = {props.selectRecipe}/> */}
-
     </Box>
   );
 }
