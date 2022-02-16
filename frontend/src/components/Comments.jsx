a9a7a2199515da2fd7928363e351fb0057a1a408
Jhanvi <83320700+JhanviShah1@users.noreply.github.com>
2022-02-16 15:13:54 -0500

    oneComment and DisplayComments

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -1,34 +1,34 @@
-import * as React from "react";
-import TextareaAutosize from "@mui/material/TextareaAutosize";
-import { elementAcceptingRef } from "@mui/utils";
-import Paper from "@mui/material/Paper";
-import { listClasses } from "@mui/material";
+// import * as React from "react";
+// import TextareaAutosize from "@mui/material/TextareaAutosize";
+// import { elementAcceptingRef } from "@mui/utils";
+// import Paper from "@mui/material/Paper";
+// import { listClasses } from "@mui/material";
 
-export default function Components(props) {
+// export default function Components(props) {
   
-  let userName = "";
-  let myString = "";
-  let users = props.comments.users;
+//   let userName = "";
+//   let myString = "";
+//   let users = props.comments.users;
 
 
-  const findUser = (comment) => { 
-    users.forEach(user => {
-      if (user.id === comment.user_id) {
-        console.log(`user id is: ${user.id} and comment.user_id is: ${comment.user_id}`);
-      }
-    })
-  }
+//   const findUser = (comment) => { 
+//     users.forEach(user => {
+//       if (user.id === comment.user_id) {
+//         console.log(`user id is: ${user.id} and comment.user_id is: ${comment.user_id}`);
+//       }
+//     })
+//   }
 
 
-  const commentsqq = props.comments.comments.map((comment) => {
+//   const commentsqq = props.comments.comments.map((comment) => {
     
    
-  });
+//   });
 
   
-   return <Paper elevation={3}>
-     {myString}
-     {userName}
+//    return <Paper elevation={3}>
+//      {myString}
+//      {userName}
      
-   </Paper>
-}
+//    </Paper>
+// }
