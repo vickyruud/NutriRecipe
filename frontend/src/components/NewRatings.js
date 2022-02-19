73a0dc5bb6d93f871226edc365bac2c8da88b986
vickyruud <aprajit.k@gmail.com>
2022-02-19 13:29:03 -0500

    add rating feature

---
+++ frontend/src/components/NewRatings.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import {FaStar} from 'react-icons/fa'
+import './newRatings.css'
+
+const NewRatings = () => {
+
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+
+
+  return (
+    <div>
+      {[...Array(5)].map((heart, i) => {
+        const ratingValue = i + 1
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+              
+                
+            /> 
+            <FaStar
+              icon="fa-solid fa-heart"
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+              className='heart'
+              color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"} size={25} />
+          </label>);
+      })}      
+    </div>
+  )
+}
+
+export default NewRatings
