4b45a342d203637a62009a3b2526b4b023234fad
Jhanvi <83320700+JhanviShah1@users.noreply.github.com>
2022-02-19 13:54:06 -0500

    Search Bar

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import DirectionsIcon from '@mui/icons-material/Directions';
+
+export default function SearchBar(props) {
+  return (
+    <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+    >
+      <IconButton sx={{ p: '10px' }} aria-label="menu">
+        {/* <MenuIcon /> */}
+      </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Recipes"
+        onChange={e=>
+      props.searchRecipe(e.target.value)}
+      
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+}
