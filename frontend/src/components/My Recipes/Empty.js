405fc0cefccce1bcf5924eb7c4f7b6ac9567b01f
vickyruud <aprajit.k@gmail.com>
2022-02-21 10:06:25 -0500

    fix myrecipe view when there's only one recipe

--- frontend/src/components/My Recipes/Empty.js
+++ frontend/src/components/My Recipes/Empty.js
@@ -17,6 +17,49 @@ const styles = {
 };
 
 export default function Empty(props) {
+
+  const generateListArray = () => {
+    let arrayOfRecipes = [];
+
+    if (props.recipes.length === 1) {
+      
+       arrayOfRecipes = props.recipes.map((recipe, index) => {
+          return ( <Grid item xs={18}>
+            <RecipeListItem
+              setSelectRecipe={props.setSelectRecipe}
+              recipe={recipe}
+              viewRecipe={props.viewRecipe}
+              onDelete={props.onDelete}
+              onEdit={props.onEdit}
+              user={props.user}
+              ratings={props.ratings}
+              recipes={props.recipes}
+            />
+          </Grid>
+          )
+        }
+        )
+      } else {
+        arrayOfRecipes = props.recipes.map((recipe, index) => (
+          <Grid item xs={4} sm={4} md={4} key={index}>
+            <RecipeListItem
+              setSelectRecipe={props.setSelectRecipe}
+              recipe={recipe}
+              viewRecipe={props.viewRecipe}
+              onDelete={props.onDelete}
+              onEdit={props.onEdit}
+              user={props.user}
+              ratings={props.ratings}
+              recipes={props.recipes}
+              />
+         </Grid>
+         
+         ))
+         
+         
+    }
+    return arrayOfRecipes
+  }
   return (
     <div>
       {props.user &&
@@ -42,26 +85,15 @@ export default function Empty(props) {
         justifyContent: "center",
         paddingTop: 3,
       }}
-    >
+      >
+        
       <Grid
         container
         spacing={{ sm: 2, md: 3 }}
         columns={{ xs: 4, sm: 5, md: 20 }}
-      >
-        {props.recipes.map((recipe, index) => (
-          <Grid item xs={7} sm={7} md={10} key={index}>
-            <RecipeListItem
-              setSelectRecipe={props.setSelectRecipe}
-              recipe={recipe}
-              viewRecipe={props.viewRecipe}
-              onDelete={props.onDelete}
-              onEdit={props.onEdit}
-              user={props.user}
-              ratings={props.ratings}
-              recipes={props.recipes}
-            />
-          </Grid>
-        ))}
+        >
+          {generateListArray()}
+     
       </Grid>
     </Box>
 </div>
