1e39be6e0a62b18fd7f843f00834d9c995cb1719
vickyruud <aprajit.k@gmail.com>
2022-02-21 10:07:58 -0500

    merge empty.js

--- frontend/src/components/My Recipes/Empty.js
+++ frontend/src/components/My Recipes/Empty.js
@@ -11,7 +11,6 @@
 // import { Button } from "@mui/material";
 
 
-<<<<<<< HEAD
 export default function Empty(props) {
 
   const generateListArray = () => {
@@ -95,95 +94,3 @@ export default function Empty(props) {
 </div>
   );
 }
-=======
-
-
-// export default function ResponsiveGrid(props) {
-
-
-    
-//   const generateListArray = () => {
-//     let arrayOfRecipes = [];
-
-//     if (props.recipes.length === 1) {
-      
-//        arrayOfRecipes = props.recipes.map((recipe, index) => {
-//           return ( <Grid item xs={12}>
-//             <RecipeListItem
-//               setSelectRecipe={props.setSelectRecipe}
-//               recipe={recipe}
-//               viewRecipe={props.viewRecipe}
-//               onDelete={props.onDelete}
-//               onEdit={props.onEdit}
-//               user={props.user}
-//               ratings={props.ratings}
-//               recipes={props.recipes}
-//             />
-//           </Grid>
-//           )
-//         }
-//         )
-//       } else {
-//         arrayOfRecipes = props.recipes.map((recipe, index) => (
-//           <Grid item xs={4} sm={4} md={4} key={index}>
-//             <RecipeListItem
-//               setSelectRecipe={props.setSelectRecipe}
-//               recipe={recipe}
-//               viewRecipe={props.viewRecipe}
-//               onDelete={props.onDelete}
-//               onEdit={props.onEdit}
-//               user={props.user}
-//               ratings={props.ratings}
-//               recipes={props.recipes}
-//               />
-//          </Grid>
-         
-//          ))
-         
-         
-//     }
-//     return arrayOfRecipes
-//   }
-      
-  
-
-      
-    
-//   const renderSearch = () => {
-//     if (props.openSearch !== false) {
-//       return <SearchBar 
-//         searchRecipe={props.searchRecipe} closeSearch={ props.closeSearch}/>
-//     } else {
-//       return (
-//         <Button variant="contained" onClick={props.showSearch}>Search Recipes</Button>
-//       )
-//       }
-//   }
-
-//   return (
-//     <div className="search-location">
-//       <br></br>
-//       {renderSearch()}
-//     <Box
-//       sx={{
-//         flexGrow: 1,
-//         display: "flex",
-//         flexDirection: "row",
-//         justifyContent: "center",
-//         padding: 5,
-//       }}
-//     >
-     
-//       <Grid
-//         container
-//         spacing={{ sm: 2, md: 3 }}
-//         columns={{ xs: 4, sm: 5, md: 20 }}
-//       >
-//         {generateListArray()}
-//       </Grid>
-//     </Box>
-//   </div>
-//   );
-// }
-
->>>>>>> b382d50e064211a694abd86bb7d277f4b5bcc495
