e6d4d76a2173af6bb8a86ba2fb7e7389799cee34
vickyruud <aprajit.k@gmail.com>
2022-02-22 09:26:56 -0500

    add avatars

--- frontend/src/components/DisplayComments.js
+++ frontend/src/components/DisplayComments.js
@@ -3,6 +3,7 @@ import OneComment from './OneComment';
 import { Container, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import CancelSharpIcon from '@mui/icons-material/CancelSharp';
+import { generateRandomAvatarOptions } from './Avatars';
 
 
 const DisplayComments = (props) => {
@@ -10,10 +11,12 @@ const DisplayComments = (props) => {
   const buttonStyle = { margin: '10px 0 0 0' }
 
 
+
+ 
   const arrayOfComments = props.comments.comments.map(comment => {
     if (props.list.id === comment.recipe_id) {
       console.log(comment.recipe_id);
-      return <OneComment comment={comment} users={props.comments.users} />      
+      return <OneComment comment={comment} users={props.comments.users}  />      
     }
   })
   return (
